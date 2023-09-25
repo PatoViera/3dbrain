23d374007b198f3ae1967a1406aa430452ce5f86
Patricio Viera Perez <pvier002@fiu.edu>
2023-09-25 17:03:37 -0400

    Updated the 3dbrain pipeline works for single participant

--- run_3dbrain_pipeline.py
+++ run_3dbrain_pipeline.py
@@ -17,32 +17,33 @@ def get_niftis(subject_id, data_dir):
     layout = BIDSLayout(data_dir)
     t1s = [f.path for f in layout.get(subject=subject_id, suffix='T1w', run='01',
                                           extension=['nii', 'nii.gz'])]
-    return t1s[0]
-############# Were causing issues when connecting the nodes ########
-## def get_rh(pial_list):
-#    """
-#    Is there a better way of doing this?
-#    """
-#    for p in pial_list:
-#        if p.endswith('rh.pial'):
-#            return p
-#
-#
-## def get_lh(pial_list):
-#    """
-#    Is there a better way of doing this?
-#    """
-#    for p in pial_list:
-#        if p.endswith('lh.pial'):
-#            return p
-###################################################################
-############# Not needed ###########################
-#def to_list(f1, f2):
-#    """
-#    Simple function to format inputs to MRIsCombine
-#    """
-#    return [f1, f2]
-####################################################
+    delimiter = ', '  # You can choose any delimiter you want
+
+    # Use the join() method to convert the list to a string
+    t1 = delimiter.join(t1s)
+    return t1
+
+def get_rh(pial_list):
+    """
+    Is there a better way of doing this?
+    """
+    for p in pial_list:
+        if p.endswith('rh.pial'):
+            return p
+
+def get_lh(pial_list):
+  """
+  Is there a better way of doing this?
+  """
+  for p in pial_list:
+      if p.endswith('lh.pial'):
+          return p
+
+def to_list(f1, f2):
+    """
+    Simple function to format inputs to MRIsCombine
+    """
+    return [f1, f2]
 
 def main(dataset, output_dir, sub_ids, work_dir):
     """
@@ -77,40 +78,48 @@ def main(dataset, output_dir, sub_ids, work_dir):
     wf.connect(subj_iterable, 'subject_id', reconall, 'subject_id')
     wf.connect(BIDSDataGrabber, 'T1_files', reconall, 'T1_files')
     
-    ############### Was causing issues with MRIsConvert due to file specificity ################
-    ## Convert each pial file to stl format.
-    #conv_rh = pe.Node(MRIsConvert(out_datatype='stl'), name='conv_rh')
-    #wf.connect(reconall, 'pial', conv_rh, 'in_file')
-    #
-    #conv_lh = pe.Node(MRIsConvert(out_datatype='stl'), name='conv_lh')
-    #wf.connect(reconall, 'pial', conv_lh, 'in_file')
-    #
-    ## Combine the two GM surface files into a brain.
-    ## I assume we want to add something to allow users to combine other labels.
-    #tolist = pe.Node(Function(function=to_list,
-    #                           input_names=['f1', 'f2'],
-    #                           output_names=['lst']),
-    #                   name='ToList')
-    #wf.connect(reconall, 'pial', tolist, 'f1')
-    #wf.connect(reconall, 'pial', tolist, 'f2')
-    #######################################################################################
-    # Modify the out_file_template to include the subject number
-    out_file_templates = [os.path.join(output_dir, f'{sub_id}_combined.pial') for sub_id in sub_ids]
-    out_file_template = out_file_templates[0]
+    # Grab each specific ".pial" file 
+    pial_grabber_lh = pe.Node(Function(function=get_lh,
+                                       input_names=['pial_list'],
+                                       output_names=['lh_pial']),
+                                name='pial_grabber_lh')
+    wf.connect(reconall, 'pial', pial_grabber_lh, 'pial_list')
+
+    pial_grabber_rh = pe.Node(Function(function=get_rh,
+                                       input_names=['pial_list'],
+                                       output_names=['rh_pial']),
+                                name='pial_grabber_rh')
+    wf.connect(reconall, 'pial', pial_grabber_rh, 'pial_list')
+
+    # Convert each ".pial" file to ".stl" format.
+    conv_lh = pe.Node(MRIsConvert(out_datatype='stl'), name='conv_lh')
+    wf.connect(pial_grabber_lh, 'lh_pial', conv_lh, 'in_file')
+
+    conv_rh = pe.Node(MRIsConvert(out_datatype='stl'), name='conv_rh')
+    wf.connect(pial_grabber_rh, 'rh_pial', conv_rh, 'in_file')
+    
+    # Combine the two GM surface files into a brain.
+    # I assume we want to add something to allow users to combine other labels.
+    tolist = pe.Node(Function(function=to_list,
+                               input_names=['f1', 'f2'],
+                               output_names=['lst']),
+                       name='ToList')
+    wf.connect(conv_lh, 'converted', tolist, 'f1')
+    wf.connect(conv_rh, 'converted', tolist, 'f2')
+    
+    for sub_id in sub_ids:
+        out_file_template = os.path.join(output_dir, sub_id, 'surf', f'sub-{sub_id}_combined.stl')
     # Combine the two GM surface files into a brain.
     comb = pe.Node(MRIsCombine(out_file=out_file_template), name='comb')
-    wf.connect(reconall, 'pial', comb, 'in_files')
-
-    # Convert the combined ".pial" file to stl format.
-    conv_bh = pe.Node(MRIsConvert(out_datatype='stl'), name='conv_bh')
-    wf.connect(comb, 'out_file', conv_bh, 'in_file')
+    wf.connect(tolist, 'lst', comb, 'in_files')
     
     # Save the relevant data into an output directory
     datasink = pe.Node(nio.DataSink(), name='datasink')
     datasink.inputs.base_directory = output_dir
-    #wf.connect(conv_rh, 'converted', datasink, 'rh_stl')
-    #wf.connect(conv_lh, 'converted', datasink, 'lh_stl')
-    wf.connect(conv_bh, 'converted', datasink, 'full_stl')
+    wf.connect(conv_rh, 'converted', datasink, 'rh_stl')
+    wf.connect(conv_lh, 'converted', datasink, 'lh_stl')
+    wf.connect(BIDSDataGrabber, 'T1_files', datasink, 'T1s')
+    wf.connect(comb, 'out_file', datasink, 'full_stl')
     
     # Run things
     # wf.run(plugin='SLURM', plugin_args={'sbatch_args': '-q pq_nbc'})
